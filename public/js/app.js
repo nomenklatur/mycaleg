6302c79a60dbc6da2057263d5929572e64e06bbc
nomenklatur <dimasekaputra@students.usu.ac.id>
2022-08-13 13:35:43 +0700

    new user form

--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,11 @@
-const sign_in_btn = document.querySelector("#sign-in-btn");
-const sign_up_btn = document.querySelector("#sign-up-btn");
-const container = document.querySelector(".container");
+const signUpButton = document.getElementById('signUp');
+const signInButton = document.getElementById('signIn');
+const container = document.getElementById('container');
 
-sign_up_btn.addEventListener("click", () => {
-  container.classList.add("sign-up-mode");
+signUpButton.addEventListener('click', () => {
+	container.classList.add("right-panel-active");
 });
 
-sign_in_btn.addEventListener("click", () => {
-  container.classList.remove("sign-up-mode");
+signInButton.addEventListener('click', () => {
+	container.classList.remove("right-panel-active");
 });
--- resources/views/login_form.blade.php
+++ resources/views/login_form.blade.php
@@ -1,82 +1,55 @@
 <!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <script
-      src="https://kit.fontawesome.com/64d58efce2.js"
-      crossorigin="anonymous"
-    ></script>
-    <link rel="stylesheet" href="css/login-register.css" />
-    <link rel="icon" href="images/logo_usu.png">
-    <title>MyCaleg | {{ $title }}</title>
-  </head>
-  <body>
-    <div class="container">
-      <div class="forms-container">
-        <div class="signin-signup">
-          <form action="#" class="sign-in-form">
-            <h2 class="title">Sign in</h2>
-            <div class="input-field">
-              <i class="fas fa-user"></i>
-              <input type="text" placeholder="Username" />
-            </div>
-            <div class="input-field">
-              <i class="fas fa-lock"></i>
-              <input type="password" placeholder="Password" />
-            </div>
-            <input type="submit" value="Login" class="btn solid" />
-          </form>
+<html lang="en" dir="ltr">
 
-          <form action="#" class="sign-up-form">
-            <h2 class="title">Sign up</h2>
-            <div class="input-field">
-              <i class="fas fa-user"></i>
-              <input type="text" placeholder="Username" />
-            </div>
-            <div class="input-field">
-              <i class="fas fa-envelope"></i>
-              <input type="email" placeholder="Email" />
-            </div>
-            <div class="input-field">
-              <i class="fas fa-lock"></i>
-              <input type="password" placeholder="Password" />
-            </div>
-            <input type="submit" class="btn" value="Sign up" />
-            </form>
-        </div>
-      </div>
+<head>
+  <meta charset="utf-8">
+  <title>MyCaleg | {{$title}}</title>
+  <link rel="icon" href="images/logo.png">
+  <link rel="stylesheet" href="css/login-register.css">
+  <link href="https://fonts.googleapis.com/css?family=Arvo" rel="stylesheet">
+	<link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css'>
+</head>
 
-      <div class="panels-container">
-        <div class="panel left-panel">
-          <div class="content">
-            <h3>New here ?</h3>
-            <p>
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Debitis,
-              ex ratione. Aliquid!
-            </p>
-            <button class="btn transparent" id="sign-up-btn">
-              Sign up
-            </button>
-          </div>
-          <img src="images/8.png" class="image" alt="" />
-        </div>
-        <div class="panel right-panel">
-          <div class="content">
-            <h3>One of us ?</h3>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
-              laboriosam ad deleniti.
-            </p>
-            <button class="btn transparent" id="sign-in-btn">
-              Sign in
-            </button>
-          </div>
-          <img src="images/7.png" class="image" alt="" />
-        </div>
-      </div>
-    </div>
+<body>
 
-    <script src="js/app.js"></script>
-  </body>
+
+<div class="container" id="container">
+	<div class="form-container sign-up-container">
+		<form action="#">
+			<h1>Buat Akun</h1>
+			<span>gunakan e-mail kamu untuk registrasi</span>
+			<input type="text" placeholder="Nama" />
+			<input type="email" placeholder="Email" />
+			<input type="password" placeholder="Password" />
+			<button>Daftar</button>
+		</form>
+	</div>
+	<div class="form-container sign-in-container">
+		<form action="#">
+			<h1>Masuk</h1>
+			<span>gunakan akun yang terdaftar untuk masuk</span>
+			<input type="email" placeholder="Email" />
+			<input type="password" placeholder="Password" />
+			<button>Masuk</button>
+		</form>
+	</div>
+	<div class="overlay-container">
+		<div class="overlay">
+			<div class="overlay-panel overlay-left">
+				<h1>Halo kamu!</h1>
+				<p>Untuk dapat menggunakan fitur tertentu silahkan masuk menggunakan akunmu</p>
+				<button class="ghost" id="signIn">Masuk</button>
+			</div>
+			<div class="overlay-panel overlay-right">
+				<h1>Selamat Datang!</h1>
+				<p>Daftarkan diri kamu agar dapat menggunakan fitur sistem</p>
+				<button class="ghost" id="signUp">Daftar</button>
+			</div>
+		</div>
+	</div>
+</div>
+
+<script src="js/app.js" charset="utf-8"></script>
+</body>
+
 </html>
