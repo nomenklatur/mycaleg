4a5f477998b6956432170d0c6b519a01d991ae78
nomenklatur <dimasekaputra2000@gmail.com>
2022-09-27 15:51:23 +0700

    footer added

--- resources/views/layout/base_layout.blade.php
+++ resources/views/layout/base_layout.blade.php
@@ -9,12 +9,13 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
     <link rel="stylesheet" href="/css/style.css">
     <link rel="icon" href="/images/logo_usu.png">
+    <script src="https://kit.fontawesome.com/2301857516.js" crossorigin="anonymous"></script>
 </head>
 <body>
   
       @include('partials.navbar')
       @yield('content')
-      
+      @include('partials.footer')
       <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
 </body>
 </html>
---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,71 @@
+<footer
+          class="text-center text-lg-start text-white mt-3 mb-0"
+          style="background-color: green"
+          >
+    <!-- Grid container -->
+    <div class="container p-1 pb-0 mb-0">
+      <!-- Section: Copyright -->
+      <section class="p-4 pt-0">
+        <div class="row d-flex align-items-center">
+          <!-- Grid column -->
+          <div class="col-md-7 col-lg-8 text-center text-md-start">
+            <!-- Copyright -->
+            <div class="p-3">
+              © 2022 Copyright:
+              <a class="text-white" href="https://dimasekaputra.netlify.app/" target="_blank"
+                 >Dimas Eka Putra</a
+                >
+            </div>
+            <!-- Copyright -->
+          </div>
+          <!-- Grid column -->
+
+          <!-- Grid column -->
+          <div class="col-md-5 col-lg-4 ml-lg-0 text-center text-md-end">
+            <!-- Facebook -->
+            <a
+              href="https://www.linkedin.com/in/masdimasekaputra/"
+               class="btn btn-outline-light btn-floating m-1"
+               class="text-white"
+               role="button"
+               target="_blank"
+               ><i class="fab fa-linkedin-in"></i
+              ></a>
+
+            <!-- Twitter -->
+            <a
+                href="https://t.me/masstah"
+               class="btn btn-outline-light btn-floating m-1"
+               class="text-white"
+               role="button"
+               target="_blank"
+               ><i class="fab fa-telegram"></i
+              ></a>
+
+            <!-- Google -->
+            <a
+               class="btn btn-outline-light btn-floating m-1"
+               class="text-white"
+               role="button"
+               target="_blank"
+               href="mailto:mycodingpersona@gmail.com?subject = Feedback"
+               ><i class="fab fa-google"></i
+              ></a>
+
+            <!-- Instagram -->
+            <a
+                href="https://www.instagram.com/masstaahh/"
+               class="btn btn-outline-light btn-floating m-1"
+               class="text-white"
+               role="button"
+               target="_blank"
+               ><i class="fab fa-instagram"></i
+              ></a>
+          </div>
+          <!-- Grid column -->
+        </div>
+      </section>
+      <!-- Section: Copyright -->
+    </div>
+    <!-- Grid container -->
+  </footer>
